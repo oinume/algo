cfc2eca4561cd838062cfb858135b4382651718f
Kazuhiro Oinuma <oinume@gmail.com>
2015-09-30 01:23:10 +0900

    Add BinaryTreeNode.Accept()

--- src/data/binary_tree.go
+++ src/data/binary_tree.go
@@ -1,8 +1,20 @@
 package data
 
+type VisitFunc func(value *Object)
+
 type BinaryTreeNode struct {
-	value *Object
-	left *BinaryTreeNode
-	right *BinaryTreeNode
+	Value     *Object
+	Left      *BinaryTreeNode
+	Right     *BinaryTreeNode
+	visitFunc VisitFunc
 }
 
+func (n *BinaryTreeNode) Accept(visitor VisitFunc) {
+	visitor(n.Value)
+	if n.Left != nil {
+		n.Left.Accept(visitor)
+	}
+	if n.Right != nil {
+		n.Right.Accept(visitor)
+	}
+}
