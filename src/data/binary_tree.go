c25d7afe474bf5208e2402b9d971181754870907
Kazuhiro Oinuma <oinume@gmail.com>
2015-10-03 01:04:42 +0900

    Create Visitor interface

--- src/data/binary_tree.go
+++ src/data/binary_tree.go
@@ -1,20 +1,21 @@
 package data
 
-type VisitFunc func(value *Object)
-
-type BinaryTreeNode struct {
-	Value     *Object
-	Left      *BinaryTreeNode
-	Right     *BinaryTreeNode
-	visitFunc VisitFunc
+type Visitor interface {
+	Visit(node *BinaryTreeNode, depth int)
 }
 
-func (n *BinaryTreeNode) Accept(visitor VisitFunc) {
-	visitor(n.Value)
+type BinaryTreeNode struct {
+	Value *Object
+	Left  *BinaryTreeNode
+	Right *BinaryTreeNode
+}
+
+func (n *BinaryTreeNode) Accept(visitor Visitor, depth int) {
+	visitor.Visit(n, depth)
 	if n.Left != nil {
-		n.Left.Accept(visitor)
+		n.Left.Accept(visitor, depth+1)
 	}
 	if n.Right != nil {
-		n.Right.Accept(visitor)
+		n.Right.Accept(visitor, depth+1)
 	}
 }
--- src/data/binary_tree_test.go
+++ src/data/binary_tree_test.go
@@ -5,11 +5,16 @@ import (
 	"testing"
 )
 
+type visitor struct{}
+
+func (v *visitor) Visit(node *BinaryTreeNode, depth int) {
+	fmt.Printf("value = %v\n", node.Value)
+}
+
 func TestBinaryTreeNode(t *testing.T) {
 	root := &BinaryTreeNode{Value: &Object{"ROOT"}}
 	root.Left = &BinaryTreeNode{Value: &Object{"1"}}
 	root.Right = &BinaryTreeNode{Value: &Object{"2"}}
-	root.Accept(func(value *Object) {
-		fmt.Printf("value = %v\n", value)
-	})
+	root.Right.Left = &BinaryTreeNode{Value: &Object{"3"}}
+	root.Accept(&visitor{}, 1)
 }
