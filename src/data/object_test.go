25227d4bcbc2e13017a51720f88410a1047cb640
Kazuhiro Oinuma <oinume@gmail.com>
2015-10-09 10:53:19 +0900

    Add methods ToString(), ToStringDefault()

--- src/data/object_test.go
+++ src/data/object_test.go
@@ -2,9 +2,17 @@ package data
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestObjectString(t *testing.T) {
 	o := NewObjectInt(1)
 	t.Log(o)
+}
+
+func TestObjectToInt(t *testing.T) {
+	assert := assert.New(t)
+	assert.Equal(1, NewObjectInt(1).ToInt())
+	assert.Equal(-1, Object{"abc"}.ToIntDefault(-1))
 }
