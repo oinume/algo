1910b3270d2e8fc930f2d6aad2dc1ad8b0a73f45
Kazuhiro Oinuma <oinume@gmail.com>
2015-12-14 13:37:55 +0900

    *Object -> Value (lots of todo)

---
+++ src/data/value.go
@@ -0,0 +1,54 @@
+package data
+
+import (
+	"fmt"
+)
+
+type Value interface {
+	Get() interface{}
+	Receive(v interface{}) error
+	ToString() string
+	ToInt() int
+}
+
+type Object struct {
+	Value interface{}
+}
+
+func (o *Object) String() string {
+	return fmt.Sprintf("Object{Value: %v}", o.Value)
+}
+
+func (o *Object) Get() interface{} {
+	return o.Value
+}
+
+func (o *Object) Receive(v interface{}) error {
+	// TODO
+	return nil
+}
+
+func (o Object) ToInt() int {
+	return o.Value.(int)
+}
+
+func (o Object) ToIntDefault(def int) int {
+	value, ok := o.Value.(int)
+	if ok {
+		return value
+	}
+	return def
+}
+
+func (o Object) ToString() string {
+	return o.Value.(string)
+}
+
+func (o Object) ToStringDefault(def string) string {
+	value, ok := o.Value.(string)
+	if ok {
+		return value
+	}
+	return def
+}
+
