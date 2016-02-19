c81c8a3d5fd17ea2639dfadf37decc5cada3c1af
Kazuhiro Oinuma <oinume@gmail.com>
2016-02-20 02:14:15 +0900

    Dummy implementation

---
+++ src/data/hash_map.go
@@ -0,0 +1,11 @@
+package data
+
+type hashMap struct {}
+
+func (h *hashMap) Put(key Value, value Value) Value {
+	return nil
+}
+
+func (h *hashMap) Get(key Value) (Value, error) {
+	return nil, nil
+}
