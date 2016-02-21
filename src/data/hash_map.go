c8c96c2dc323b91f9c46139664c1d44f11de9cd9
Kazuhiro Oinuma <oinume@gmail.com>
2016-02-21 22:16:54 +0900

    Implement Get()

--- src/data/hash_map.go
+++ src/data/hash_map.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"container/list"
 )
 
 const defaultMaxSize = 100
@@ -9,7 +10,12 @@ const defaultMaxSize = 100
 type hashMap struct {
 	maxSize int
 	size int
-	data []Value
+	data []*list.List
+}
+
+type item struct {
+	key Value
+	value Value
 }
 
 func NewHashMap(maxSize int) Map {
@@ -19,31 +25,61 @@ func NewHashMap(maxSize int) Map {
 	return &hashMap{
 		maxSize: maxSize,
 		size: 0,
-		data: make([]Value, maxSize),
+		data: make([]*list.List, maxSize),
 	}
 }
 
 func (h *hashMap) Put(key Value, value Value) Value {
-	k, ok := key.(Hashable)
-	var hashCode int
-	if ok {
-		hashCode = k.HashCode()
+	index := h.getIndex(key)
+	if h.data[index] == nil {
+		// Put as new
+		l := list.New()
+		l.PushBack(&item{key: key, value: value})
+		h.data[index] = l
+		h.size++
 	} else {
-		hashCode = h.hashCode(key)
+		l := h.data[index]
+		for e := l.Front(); e != nil; e = e.Next() {
+			if i := e.Value.(*item); i.key.Get() == key.Get() {
+				// Replace an old item with new one
+				l.Remove(e)
+				l.PushBack(&item{key: key, value: value})
+				return i.value
+			}
+		}
 	}
-	loc := hashCode % h.maxSize
-	h.data[loc] = value
-	return value
+	return nil
 }
 
 func (h *hashMap) Get(key Value) (Value, error) {
-	return nil, nil
+	index := h.getIndex(key)
+	if h.data[index] == nil {
+		return nil, fmt.Errorf("not found")
+	}
+	list := h.data[index]
+	for e := list.Front(); e != nil; e = e.Next() {
+		if i := e.Value.(*item); i.key.Get() == key.Get() {
+			return i.value, nil
+		}
+	}
+	return nil, fmt.Errorf("not found")
 }
 
-func (h *hashMap) hashCode(v Value) int {
+func (h *hashMap) calculateHashCode(v Value) int {
 	result := 0
 	for _, s := range fmt.Sprint(v) {
 		result += int(s)
 	}
 	return result
+}
+
+func (h *hashMap) getIndex(key Value) int {
+	k, ok := key.(Hashable)
+	var hashCode int
+	if ok {
+		hashCode = k.HashCode()
+	} else {
+		hashCode = h.calculateHashCode(key)
+	}
+	return hashCode % h.maxSize
 }
