c899afcd473f192c69bd70d7a07b55e22c21b60c
Kazuhiro Oinuma <oinume@gmail.com>
2018-06-02 14:11:41 +0900

    Add more tests

--- src/datastructure/hash_table/open_addressing_test.go
+++ src/datastructure/hash_table/open_addressing_test.go
@@ -8,6 +8,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestBucketKey_HashCode(t *testing.T) {
+	a := assert.New(t)
+	testCases := []struct {
+		key1         types.Value
+		key2         types.Value
+		sameHashCode bool
+	}{
+		{key1: &types.Object{1}, key2: &types.Object{1}, sameHashCode: true},
+		{key1: &types.Object{"a"}, key2: &types.Object{"b"}, sameHashCode: false},
+		{key1: &types.Object{"abc"}, key2: &types.Object{"cba"}, sameHashCode: true},
+	}
+	for _, tc := range testCases {
+		key1, key2 := &bucketKey{data: tc.key1}, &bucketKey{data: tc.key2}
+		a.Equalf(
+			tc.sameHashCode,
+			key1.HashCode() == key2.HashCode(),
+			"sameHashCode=%v", tc.sameHashCode,
+		)
+	}
+}
+
 func TestOpenAddressing_Put(t *testing.T) {
 	a := assert.New(t)
 	r := require.New(t)
@@ -28,6 +49,10 @@ func TestOpenAddressing_Put(t *testing.T) {
 		a.EqualValues(tc.wantReturn, ret)
 	}
 	a.Equal(2, hashTable.Size())
+}
+
+func TestOpenAddressing_Put_Rehash(t *testing.T) {
+	// TODO: implment
 }
 
 func TestOpenAddressing_Get(t *testing.T) {
