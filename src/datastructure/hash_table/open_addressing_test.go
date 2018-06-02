bca94595be3d7a4377b7e111569dc8477cf209a5
Kazuhiro Oinuma <oinume@gmail.com>
2018-06-02 13:57:49 +0900

    Use table driven test

--- src/datastructure/hash_table/open_addressing_test.go
+++ src/datastructure/hash_table/open_addressing_test.go
@@ -18,15 +18,9 @@ func TestOpenAddressing_Put(t *testing.T) {
 		value      types.Value
 		wantReturn types.Value
 	}{
-		{
-			key: &types.Object{1}, value: &types.Object{10}, wantReturn: nil,
-		},
-		{
-			key: &types.Object{2}, value: &types.Object{20}, wantReturn: nil,
-		},
-		{
-			key: &types.Object{2}, value: &types.Object{30}, wantReturn: &types.Object{20},
-		},
+		{key: &types.Object{1}, value: &types.Object{10}, wantReturn: nil},
+		{key: &types.Object{2}, value: &types.Object{20}, wantReturn: nil},
+		{key: &types.Object{2}, value: &types.Object{30}, wantReturn: &types.Object{20}},
 	}
 	for _, tc := range testCases {
 		ret, err := hashTable.Put(tc.key, tc.value)
@@ -39,13 +33,19 @@ func TestOpenAddressing_Put(t *testing.T) {
 func TestOpenAddressing_Get(t *testing.T) {
 	a := assert.New(t)
 	r := require.New(t)
-
 	hashTable := NewOpenAddressing()
-	hashTable.Put(&types.Object{1}, &types.Object{10})
-	hashTable.Put(&types.Object{2}, &types.Object{20})
-	a.Equal(2, hashTable.Size())
 
-	actual, err := hashTable.Get(&types.Object{2})
-	r.NoError(err)
-	a.Equal(&types.Object{20}, actual)
+	testCases := []struct {
+		key   types.Value
+		value types.Value
+	}{
+		{key: &types.Object{1}, value: &types.Object{10}},
+		{key: &types.Object{2}, value: &types.Object{20}},
+	}
+	for _, tc := range testCases {
+		_, err := hashTable.Put(tc.key, tc.value)
+		r.NoError(err)
+		actual, err := hashTable.Get(tc.key)
+		a.Equal(tc.value, actual)
+	}
 }
