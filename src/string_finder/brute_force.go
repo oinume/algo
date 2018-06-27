2f8b9e04dc94a9b2730a71a978bb9009a8915fa3
Kazuhiro Oinuma <oinume@gmail.com>
2018-06-27 10:50:31 +0900

    Implement brute force find

--- src/string_finder/brute_force.go
+++ src/string_finder/brute_force.go
@@ -8,5 +8,25 @@ func NewBruteForce() *BruteForce {
 }
 
 func (bf *BruteForce) Find(text, pattern string) int {
-	return -1
+	textRunes := []rune(text)
+	patternRunes := []rune(pattern)
+	var textPos, patternPos int
+	for textPos, patternPos = 0, 0; textPos < len(textRunes) && patternPos < len(patternRunes); {
+		// Compare text and pattern char
+		if textRunes[textPos] == patternRunes[patternPos] {
+			textPos++
+			patternPos++
+		} else {
+			// Not matched
+			//
+			textPos = textPos - patternPos + 1 // Make textPos advance with 1 step
+			patternPos = 0                     // Reset pattern position
+		}
+	}
+
+	if patternPos == len(patternRunes) {
+		return textPos - patternPos
+	} else {
+		return -1
+	}
 }
