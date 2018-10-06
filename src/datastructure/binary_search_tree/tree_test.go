eb66cd72ae8ab7a6c17c2f240d3d7f69b6ce11af
Kazuhiro Oinuma <oinume@gmail.com>
2018-10-07 01:16:11 +0900

    Add tests for Remove

--- src/datastructure/binary_search_tree/tree_test.go
+++ src/datastructure/binary_search_tree/tree_test.go
@@ -120,6 +120,158 @@ func TestTree_Insert_Exist(t *testing.T) {
 }
 
 func TestTree_Remove(t *testing.T) {
-	//a := assert.New(t)
-	//r := require.New(t)
+	a := assert.New(t)
+	r := require.New(t)
+	tests := map[string]struct {
+		setupTree        func() *Tree
+		target           int64
+		removedAfterTree func() *Tree
+	}{
+		//     5
+		//  ／    ＼
+		// 3       6
+		//  ＼
+		//    4
+		"remove leaf": {
+			setupTree: func() *Tree {
+				tree := New(NewNode(5))
+				left := NewNode(3)
+				tree.root.left = left
+				left.right = NewNode(4)
+				tree.root.right = NewNode(6)
+				return tree
+			},
+			target: 4,
+			removedAfterTree: func() *Tree {
+				tree := New(NewNode(5))
+				tree.root.left = NewNode(3)
+				tree.root.right = NewNode(6)
+				return tree
+			},
+		},
+		//          9
+		//        ／  ＼
+		//       5     14
+		//     ／
+		//    3
+		//  ／  ＼
+		// 1     4
+		"remove node with left child": {
+			setupTree: func() *Tree {
+				tree := New(NewNode(9))
+				tree.root.left = NewNode(5)
+				tree.root.right = NewNode(14)
+				left2 := NewNode(3)
+				left2.left = NewNode(1)
+				left2.right = NewNode(4)
+				tree.root.left.left = left2
+				return tree
+			},
+			target: 5,
+			removedAfterTree: func() *Tree {
+				tree := New(NewNode(9))
+				left := NewNode(3)
+				left.left = NewNode(1)
+				left.right = NewNode(4)
+				tree.root.left = left
+				tree.root.right = NewNode(14)
+				return tree
+			},
+		},
+		//       9
+		//     ／  ＼
+		//    2     10
+		//            ＼
+		//             17
+		//           ／  ＼
+		//          13    19
+		"remove node with right child": {
+			setupTree: func() *Tree {
+				tree := New(NewNode(9))
+				tree.root.left = NewNode(2)
+				tree.root.right = NewNode(10)
+				right2 := NewNode(17)
+				right2.left = NewNode(13)
+				right2.right = NewNode(19)
+				tree.root.right.right = right2
+				return tree
+			},
+			target: 10,
+			removedAfterTree: func() *Tree {
+				tree := New(NewNode(9))
+				tree.root.left = NewNode(2)
+				right := NewNode(17)
+				right.left = NewNode(13)
+				right.right = NewNode(19)
+				tree.root.right = right
+				return tree
+			},
+		},
+		"remove node with left and right child": {
+			//           20
+			//         ／   ＼
+			//        7      23
+			//     ／   ＼     ＼
+			//    4      18     29
+			//  ／  ＼   ／
+			// 2     5  10
+			//            ＼
+			//             15
+			setupTree: func() *Tree {
+				tree := New(NewNode(20))
+				tree.root.left = NewNode(7)
+				tree.root.right = NewNode(23)
+
+				left7 := NewNode(4)
+				left7.left = NewNode(2)
+				left7.right = NewNode(5)
+				tree.root.left.left = left7
+
+				right7 := NewNode(18)
+				right7.left = NewNode(10)
+				right7.left.right = NewNode(15)
+				tree.root.left.right = right7
+
+				tree.root.right.right = NewNode(29)
+
+				return tree
+			},
+			target: 7,
+			//           20
+			//         ／   ＼
+			//        10     23
+			//     ／   ＼     ＼
+			//    4      18     29
+			//  ／  ＼   ／
+			// 2     5  15
+			removedAfterTree: func() *Tree {
+				tree := New(NewNode(20))
+				tree.root.left = NewNode(10)
+				tree.root.right = NewNode(23)
+
+				left10 := NewNode(4)
+				left10.left = NewNode(2)
+				left10.right = NewNode(5)
+				tree.root.left.left = left10
+
+				right10 := NewNode(18)
+				right10.left = NewNode(15)
+				tree.root.left.right = right10
+
+				tree.root.right.right = NewNode(29)
+
+				return tree
+			},
+		},
+		// TODO: Add test with left and right child
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			tree := test.setupTree()
+			got, err := tree.Remove(test.target)
+			r.NoError(err, "target is %v", test.target)
+			a.Equal(test.target, got.Value())
+			a.Equal(test.removedAfterTree(), tree)
+		})
+	}
 }
