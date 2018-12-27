a64c955c7e79dda77e4485a3d074a094d7163975
Kazuhiro Oinuma <oinume@gmail.com>
2018-12-27 10:48:43 +0900

    Implement InsertNonFull

--- datastructure/btree/node.go
+++ datastructure/btree/node.go
@@ -1,8 +1,62 @@
 package btree
 
+import "io"
+
 type Node struct {
+	minDegree int
+	leaf      bool
+	keys      []int64
 }
 
-func NewNode(order int) *Node {
-	return &Node{}
+func NewNode(minDegree int, leaf bool) *Node {
+	return &Node{
+		minDegree: minDegree,
+		leaf:      leaf,
+		keys:      nil,
+	}
+}
+
+func (n *Node) IsLeaf() bool {
+	return n.leaf
+}
+
+func (n *Node) Children() []*Node {
+	return nil
+}
+
+func (n *Node) InsertNonFull(value int64) {
+	if n.IsLeaf() {
+		for i, key := range n.keys {
+			if value < key {
+				// Insert value at i
+				n.keys = append(n.keys[:i], append([]int64{value}, n.keys[i:]...)...)
+				return
+			}
+		}
+		n.keys = append(n.keys, value)
+	} else {
+		// TODO
+	}
+	/*
+		def insert_nonfull(self, k):
+			if self.is_leaf:
+				i = 0
+				for i in xrange(len(self)):
+					if k < self.keys[i]:
+						self.keys.insert(i, k)
+						return self
+				self.keys.append(k)
+			else:
+				i = self.locate_subtree(k)
+				c = self.children[i]
+				if (len(c) == 2 * self.t - 1):
+					self.split_child(i, c)
+					if k > self.keys[i]:
+						c = self.children[i + 1]
+				c.insert_nonfull(k)
+	*/
+}
+
+func (n *Node) Dump(w io.Writer) {
+
 }
--- datastructure/btree/tree.go
+++ datastructure/btree/tree.go
@@ -1,29 +1,47 @@
 package btree
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 var ErrAlreadyExists = fmt.Errorf("the key already exists")
 
 type Tree struct {
-	root Node
+	root      *Node
+	minDegree int
 }
 
-func NewTree(order int) *Tree {
+func NewTree(minDegree int) *Tree {
 	return &Tree{
-		root: NewNode(order),
+		root:      NewNode(minDegree, true),
+		minDegree: minDegree,
 	}
+}
+
+func (t *Tree) Insert(value int64) {
+	r := t.root
+	if len(r.Children()) == 2*t.minDegree-1 {
+		// TODO
+	} else {
+		r.InsertNonFull(value)
+	}
+}
+
+func (t *Tree) Dump(w io.Writer) {
+	t.root.Dump(w)
 }
 
 /*
 class BTree:
     def __init__(self, t=2):
-        self.t = t
+        self.t = t # t is minimum degree
         self.root = BTree.Node(t)
         self.root.is_leaf = True
 
     def insert(self, k):
         r = self.root
-        if len(r) == 2 * self.t - 1:
+        if len(r) == 2 * self.t - 1: #
             s = BTree.Node(self.t)
             s.children.append(r)
             s.split_child(0, r)
