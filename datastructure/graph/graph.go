f54319b8b0e0445c21300ada1b3e6f32c60adb94
Kazuhiro Oinuma <oinume@gmail.com>
2019-04-05 20:50:45 +0900

    Add vertexSet and implement Edges method

--- datastructure/graph/graph.go
+++ datastructure/graph/graph.go
@@ -7,12 +7,23 @@ type Edge struct {
 	end   Vertex
 }
 
-type Graph struct{}
+type Graph struct {
+	// All vertexes this Graph has
+	vertexes *vertexSet
+	// Edges per vertex
+	edges map[*Vertex][]*Edge
+}
 
 func New() *Graph {
-	return &Graph{}
+	return &Graph{
+		vertexes: NewVertexSet(100),
+		edges:    make(map[*Vertex][]*Edge, 100),
+	}
 }
 
 func (g *Graph) Edges(v *Vertex) []*Edge {
+	if edges, ok := g.edges[v]; ok {
+		return edges
+	}
 	return nil
 }
