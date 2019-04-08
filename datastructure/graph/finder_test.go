20b81e569506c15fb80cd2da56f2200c4565a9aa
Kazuhiro Oinuma <oinume@gmail.com>
2019-04-08 11:23:31 +0900

    Add newGraph method

--- datastructure/graph/finder_test.go
+++ datastructure/graph/finder_test.go
@@ -1,6 +1,9 @@
 package graph
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // https://www.geeksforgeeks.org/graph-data-structure-and-algorithms/
 func Test_dfsFinder_Find(t *testing.T) {
@@ -14,4 +17,29 @@ func Test_dfsFinder_Find(t *testing.T) {
 	if found := f.Find(graph, v1, v2); !found {
 		t.Errorf("v1 not found somehow")
 	}
+}
+
+/*
+　　　A
+　　／ ＼
+　B　ー　C
+／
+D
+*/
+func newGraph() *Graph {
+	g := New()
+	v := make([]*Vertex, 4)
+	for i := 0; i < len(v); i++ {
+		v[i] = NewVertex(fmt.Sprint(i))
+	}
+	edges := map[*Vertex][]*Edge{
+		v[0] /* A */ : {NewEdge(v[0], v[1]), NewEdge(v[0], v[2])},
+		v[1] /* B */ : {NewEdge(v[1], v[2]), NewEdge(v[1], v[3])},
+		v[2] /* C */ : {NewEdge(v[2], v[0]), NewEdge(v[2], v[1])},
+		v[3] /* D */ : {NewEdge(v[3], v[1])},
+	}
+	for v, e := range edges {
+		g.AddVertexWithEdges(v, e)
+	}
+	return g
 }
