ed2a0f7f6690a5585d693c6663911f2c1eb3cb96
Kazuhiro Oinuma <oinume@gmail.com>
2026-03-20 22:48:05 +0900

    rename

--- datastructure/graph/finder_test.go
+++ datastructure/graph/finder_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 // https://www.geeksforgeeks.org/graph-data-structure-and-algorithms/
-func Test_dfsFinder_Find_2V(t *testing.T) {
+func Test_dfsFinder_Find_2N(t *testing.T) {
 	graph := New[string]()
-	v1 := NewNode("1")
-	v2 := NewNode("2")
-	graph.AddEdge(v1, v2)
+	n1 := NewNode("1")
+	n2 := NewNode("2")
+	graph.AddEdge(n1, n2)
 
 	tests := map[string]struct {
 		finder Finder[string]
@@ -25,26 +25,26 @@ func Test_dfsFinder_Find_2V(t *testing.T) {
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			if found := test.finder.Find(graph, v1, v2, &nopVisitor[string]{}); !found {
-				t.Errorf("v1 not found somehow")
+			if found := test.finder.Find(graph, n1, n2, &nopVisitor[string]{}); !found {
+				t.Errorf("n1 not found somehow")
 			}
 		})
 	}
 }
 
 // https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
-func Test_dfsFinder_Find_4V(t *testing.T) {
-	v := make([]*Node[string], 4)
-	for i := 0; i < len(v); i++ {
-		v[i] = NewNode(fmt.Sprint(i))
+func Test_dfsFinder_Find_4N(t *testing.T) {
+	nodes := make([]*Node[string], 4)
+	for i := 0; i < len(nodes); i++ {
+		nodes[i] = NewNode(fmt.Sprint(i))
 	}
 	graph := New[string]()
-	graph.AddEdge(v[0], v[1])
-	graph.AddEdge(v[0], v[2])
-	graph.AddEdge(v[1], v[2])
-	graph.AddEdge(v[2], v[0])
-	graph.AddEdge(v[2], v[3])
-	graph.AddEdge(v[3], v[3])
+	graph.AddEdge(nodes[0], nodes[1])
+	graph.AddEdge(nodes[0], nodes[2])
+	graph.AddEdge(nodes[1], nodes[2])
+	graph.AddEdge(nodes[2], nodes[0])
+	graph.AddEdge(nodes[2], nodes[3])
+	graph.AddEdge(nodes[3], nodes[3])
 
 	tests := map[string]struct {
 		finder           Finder[string]
@@ -71,8 +71,8 @@ func Test_dfsFinder_Find_4V(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			lv := &listVisitor[string]{}
-			if found := test.finder.Find(graph, v[2], v[3], lv); !found {
-				t.Errorf("%v not found somehow", v[3])
+			if found := test.finder.Find(graph, nodes[2], nodes[3], lv); !found {
+				t.Errorf("%v not found somehow", nodes[3])
 			}
 			if got, want := lv.list, test.wantVisitedNodes; !reflect.DeepEqual(got, want) {
 				t.Errorf("visiting node is not correct: got = %+v, want = %+v", got, want)
